52c83f40b4844e3dc9cba8c87e36ad41b17d47a6
khadizaliza <khadizaakter19836@gmail.com>
2022-05-10 22:55:35 +0600

    end

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k2nsb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-//console.log(uri);
+console.log(uri);
 async function run() {
     try{
         await client.connect();
